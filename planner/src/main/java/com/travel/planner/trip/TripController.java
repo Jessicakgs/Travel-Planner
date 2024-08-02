b209cc4664d60536c5c9df6201768f973235656b
Jessicakgs <jessicakauane20@gmail.com>
2024-08-01 22:30:04 -0300

    EndPoint updateTrip

--- planner/src/main/java/com/travel/planner/trip/TripController.java
+++ planner/src/main/java/com/travel/planner/trip/TripController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -36,6 +38,21 @@ public class TripController {
         return trip.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Trip> updateTrip(@PathVariable UUID id, @RequestBody TripRequest payload){
+        Optional<Trip> trip = this.repository.findById(id);
 
+        if(trip.isPresent()) {
+            Trip boatTrip = trip.get();
+            boatTrip.setStartsAt(LocalDateTime.parse(payload.starts_at(), DateTimeFormatter.ISO_DATE_TIME));
+            boatTrip.setEndsAt(LocalDateTime.parse(payload.ends_at(), DateTimeFormatter.ISO_DATE_TIME));
+            boatTrip.setDestination(payload.destination());
+
+            this.repository.save(boatTrip);
+
+            return ResponseEntity.ok(boatTrip);
+        }
+        return ResponseEntity.notFound().build();
+    }
 
 }
